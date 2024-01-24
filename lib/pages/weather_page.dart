64d148ed36072ab14049489929fb9c0aac778936
Maksim11011 <mvbspb@bk.ru>
2024-01-24 19:53:49 +0300

    weather page

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/services/weather_services.dart';
+
+import '../models/weather_model.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  // api key
+  final _weatherService = WeatherService('a4474902fbb41c7e3bc66b9ac41580d9');
+  Weather? _weather;
+
+  // fetch weather
+  _fetchWeather() async {
+    // get the current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    // get weather for city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+
+    // any errors
+    catch (e) {
+      print(e);
+    }
+  }
+
+  // weather animations
+
+  // init state
+  @override
+  void initState() {
+    super.initState();
+
+    //fetch weather on startup
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // city name
+            Text(_weather?.cityName ?? "Загрузка города.."),
+
+            // temperature
+            Text('${_weather?.temperature.round()}°C')
+          ],
+        ),
+      ),
+    );
+  }
+}
