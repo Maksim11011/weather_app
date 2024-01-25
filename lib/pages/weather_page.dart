a9b8d1991839833d9c4a04bf13e4e4300c41dfa1
Maksim11011 <mvbspb@bk.ru>
2024-01-25 18:24:02 +0300

    add Lottie animations

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weather_app/services/weather_services.dart';
 
 import '../models/weather_model.dart';
@@ -35,6 +36,29 @@ class _WeatherPageState extends State<WeatherPage> {
   }
 
   // weather animations
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sun.json'; // defaulf to sunny
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/clouds.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thundershtorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sun.json';
+      default:
+        return 'assets/sun.json';
+    }
+  }
 
   // init state
   @override
@@ -48,15 +72,30 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade500,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // city name
-            Text(_weather?.cityName ?? "Загрузка города.."),
+            Text(
+              _weather?.cityName ?? "Загрузка города..",
+              style: const TextStyle(fontSize: 30),
+            ),
+
+            // animatoin
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
 
             // temperature
-            Text('${_weather?.temperature.round()}°C')
+            Text(
+              '${_weather?.temperature.round()}°C',
+              style: const TextStyle(
+                fontSize: 25,
+              ),
+            ),
+
+            // weather conditions
+            Text(_weather?.mainCondition ?? "")
           ],
         ),
       ),
--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -5,6 +5,7 @@ import '../models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
+  // ignore: constant_identifier_names
   static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
   final String apiKey;
 
